349dffd145905ba57b36382a4fc93805e968b11e
Frank Denzer <fdenzer@gmail.com>
2014-04-15 10:36:25 +0200

    refactor lifecycle

--- src/de/fhworms/inf1743/CoffeDone/ButtonAndSpinner.java
+++ src/de/fhworms/inf1743/CoffeDone/ButtonAndSpinner.java
@@ -11,26 +11,21 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 /**
- * @author: Frank Denzer
+ * @author Frank Denzer
  */
 public class ButtonAndSpinner extends Activity {
 
     private MediaPlayer mp;
     private ArrayAdapter adapter;
 
+    // debugging
+    private static final String TAG = ButtonAndSpinner.class.getName();
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // media source: These sound files are music and sound effects that are
-        // used in the annual presentation of The Wonders of Physics. You may
-        // download them for your personal use. You may distribute them for non-
-        // commercial purposes, provided their source is acknowledged. Permission
-        // is required if you wish to use them for commercial purposes.
-    	// @see: http://sprott.physics.wisc.edu/wop/sounds/
-
-        mp = MediaPlayer.create(this, R.raw.gong);
         Spinner sp = (Spinner) findViewById(R.id.voiceSelector);
         adapter = ArrayAdapter.createFromResource(
                 this, R.array.voiceType, android.R.layout.simple_spinner_item);
@@ -40,7 +35,23 @@ public class ButtonAndSpinner extends Activity {
         sp.setOnItemSelectedListener(new OnVoiceTypeSelected());
     }
 
-    public void startMediaPlayback(View v) {
+    @Override
+    public void onStart() {
+        // media source: These sound files are music and sound effects that are
+        // used in the annual presentation of The Wonders of Physics. You may
+        // download them for your personal use. You may distribute them for non-
+        // commercial purposes, provided their source is acknowledged. Permission
+        // is required if you wish to use them for commercial purposes.
+        // @see: http://sprott.physics.wisc.edu/wop/sounds/
+        mp = MediaPlayer.create(this, R.raw.gong);
+    }
+
+    @Override
+    public void onStop() {
+        mp.release();
+    }
+
+    public void startMediaPlayback(View v)
         mp.start();
     }
 
@@ -51,7 +62,7 @@ public class ButtonAndSpinner extends Activity {
             Toast.makeText(parentView.getContext(),
                     parentView.getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show();
             if (position == 0) {
-		// source: own voice
+                // source: own voice
                 mp = MediaPlayer.create(parentView.getContext(), R.raw.male_coffee_done);
             } else {
                 mp = MediaPlayer.create(parentView.getContext(), R.raw.gong);
